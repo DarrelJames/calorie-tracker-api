cc523ec927456f77bdadf0a65a95bf59cc4a230a
Darrel Castellano <dcastellano2007@gmail.com>
2020-01-11 12:06:27 -0800

    Create crud actions

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,2 +1,27 @@
 class LogsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    logs = current_user.logs
+    render json: logs
+  end
+
+  def show
+    log = Log.find(params:id)
+    authorize_user_resource(log)
+    render_resource(log)
+  end
+
+  def create
+    log = Log.new(log_params)
+    log.user = current_user
+    log.save
+    render_resource(log)
+  end
+
+  private
+
+  def log_params
+    params.require(:log).permit(:date)
+  end
 end
