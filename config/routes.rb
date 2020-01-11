a8d3e0a8b735ca5511078a96b56bbefbe38a45b8
Darrel Castellano <dcastellano2007@gmail.com>
2020-01-11 11:14:38 -0800

    configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
   resources :logs
-  devise_for :users
+  devise_for :users,
+             path: '',
+             path_names: {
+               sign_in: 'login',
+               sign_out: 'logout',
+               registration: 'signup'
+             },
+             controllers: {
+               sessions: 'sessions',
+               registrations: 'registrations'
+             }
   root to: "home#index"
+
+  resources :users do
+    resource :logs
+  end
 end
