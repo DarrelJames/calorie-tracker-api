87cd3760b5c0ad5958e8501018d21cdbd2e72c73
Darrel Castellano <dcastellano2007@gmail.com>
2020-01-16 19:46:22 -0800

    Add relationships to models

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,5 @@
 class Entry < ApplicationRecord
+  belongs_to :log
+  has_many :entries_foods
+  has_many :foods, through: :entries_foods
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,4 @@
 class Food < ApplicationRecord
+  has_many :entries_foods
+  has_many :entries, through: :entries_foods
 end
