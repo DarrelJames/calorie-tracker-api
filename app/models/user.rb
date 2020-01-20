f5cf576f04ef11f01f4c6781931e341ff9b89f28
Darrel Castellano <dcastellano2007@gmail.com>
2020-01-19 22:18:25 -0800

    validate name presence true

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :logs
+  validates :name, presence: true
   devise :database_authenticatable,
           :registerable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
