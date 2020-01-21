a14f2bfd72449e72c08eccf1eed597cb12c4d09a
Darrel Castellano <dcastellano2007@gmail.com>
2020-01-20 20:24:22 -0800

    generate goal resource and setup relationship between user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :logs
+  has_one :goal
   validates :name, presence: true
   devise :database_authenticatable,
           :registerable,
