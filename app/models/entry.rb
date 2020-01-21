936f614abbad735eebc585859a3c242198ae6d14
Darrel Castellano <dcastellano2007@gmail.com>
2020-01-20 19:53:27 -0800

    Added custom foods_attributes= setter method

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,4 +2,11 @@ class Entry < ApplicationRecord
   belongs_to :log
   has_many :entries_foods
   has_many :foods, through: :entries_foods
+
+  def foods_attributes=(foods_attributes)
+    foods_attributes.each do |food_attributes|
+      food = Food.find_or_create_by(food_attributes)
+      self.foods << food
+    end
+  end
 end
