dbdbfc7904b0ab057ccf80bbe389fbc4684a665c
Darrel Castellano <dcastellano2007@gmail.com>
2020-01-21 05:24:59 -0800

    create update action and strong params

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,2 +1,21 @@
 class GoalsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    goal = current_user.goal
+    render json: goal
+  end
+
+  def update
+    goal = Goal.find(params[:id])
+    authorize_user_resource(goal)
+    goal.update(goal_params)
+    render_resource(goal)
+  end
+
+  private
+
+  def goal_params
+    params.require(:goal).permit(:carbs, :fat, :protein, :calories)
+  end
 end
