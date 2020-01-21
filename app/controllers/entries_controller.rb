ebf51ceae1448db1b2df6f1a34dd656ef275b766
Darrel Castellano <dcastellano2007@gmail.com>
2020-01-20 19:35:25 -0800

    authenticate user. created create action and strong params

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,16 @@
 class EntriesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    binding.pry
+    entry = Entry.new(entry_params)
+    entry.save
+    render_resource(entry)
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:category, foods_attributes: [:label, :fat, :protein, :calories, :carbs])
+  end
 end
