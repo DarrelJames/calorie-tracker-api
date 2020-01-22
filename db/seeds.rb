4c429d53e4f6362b211d688aa877cca13dbedfb5
Darrel Castellano <dcastellano2007@gmail.com>
2020-01-21 18:18:46 -0800

    Create seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+u = User.create(name: 'dj', email: 'test@email.com', password: 'password')
+
+categories = ['Breakfast', 'Lunch', 'Dinner', 'Snacks']
+
+a = u.logs.create(date: '2020-01-19')
+b = u.logs.create(date: '2020-01-20')
+c = u.logs.create(date: '2020-01-21')
+
+
+5.times do
+  e = a.entries.create(category: categories.sample)
+
+  e.foods.create(label: Faker::Food.sushi)
+end
+5.times do
+  e = b.entries.create(category: categories.sample)
+
+  e.foods.create(label: Faker::Food.sushi)
+end
+5.times do
+  e = c.entries.create(category: categories.sample)
+
+  e.foods.create(label: Faker::Food.sushi)
+end
