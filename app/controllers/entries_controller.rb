a16904d351f4dc9d2ff72046dcb70b7b8d9ceef6
Darrel Castellano <dcastellano2007@gmail.com>
2020-01-23 20:05:59 -0800

    Add update and destroy actions and strong params

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,16 +2,30 @@ class EntriesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    l = Log.last
-    entry = Entry.new(entry_params)
-    entry.log = l
+    log = Log.find(params[:log_id])
+    entry = log.entries.build(entry_params)
     entry.save
-    render_resource(entry)
+    render_resource(log)
+  end
+
+  def update
+    entry = Entry.find(params[:id])
+    authorize_user_resource(entry.log)
+    entry.update(entry_params)
+    render_resource(entry.log)
+  end
+
+  def destroy
+    entry = Entry.find(params[:id])
+    log = entry.log
+    authorize_user_resource(entry.log)
+    entry.destroy
+    render_resource(log)
   end
 
   private
 
   def entry_params
-    params.require(:entry).permit(:category, foods_attributes: [:label, :fat, :protein, :calories, :carbs])
+    params.require(:entry).permit(:category, :servings, foods_attributes: [:label, :fat, :protein, :calories, :carbs])
   end
 end
