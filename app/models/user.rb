5638bf803d1cb10b9a5c9e627f1c9be35cbcc202
Darrel Castellano <dcastellano2007@gmail.com>
2020-01-23 18:56:33 -0800

    Add dependent destroy for goal

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :logs
-  has_one :goal
+  has_one :goal, dependent: :destroy
   validates :name, presence: true
   devise :database_authenticatable,
           :registerable,
